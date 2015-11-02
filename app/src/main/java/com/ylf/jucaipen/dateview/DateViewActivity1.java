d4067efdc9eac362698785dd70b68c38d8cb8448
langfei yang <185601452@qq.com>
2015-11-02 15:29:27 +0800

    百分比布局

--- app/src/main/java/com/ylf/jucaipen/dateview/DateViewActivity1.java
+++ app/src/main/java/com/ylf/jucaipen/dateview/DateViewActivity1.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-
 /**
  * Created by Administrator on 2015/11/2.
  */
 public class DateViewActivity1 extends Activity {
-    private Button btn_born;
     private LayoutInflater inflater;
 
     @Override
@@ -22,18 +20,7 @@ public class DateViewActivity1 extends Activity {
 
     }
 
-    private void initView() {
-        btn_born= (Button) findViewById(R.id.btn_born);
-        inflater=LayoutInflater.from(this);
-        btn_born.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Dialog d=new Dialog(DateViewActivity1.this);
-                View dateView=inflater.inflate(R.layout.ui_date_dialog,null);
-                d.setContentView(dateView);
-                d.show();
+    private void initView(){
 
-            }
-        });
-    }
+    };
 }
